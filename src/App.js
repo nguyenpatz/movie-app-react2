35801cbc2cd9b6849a89d6219ee263870db05206
nguyenpat <nguyenpatzz@gmail.com>
2022-05-15 17:23:33 +0700

    update footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import MovieListHeading from "./components/MovieListHeading";
+import MovieList from "./components/MovieList";
+import SearchBox from "./components/SearchBox";
+import AddToFavourites from "./components/AddToFavourites";
+import Filter from "./components/Filter";
+import Github from "./Github.svg";
+// import AddToFavourites from "./components/AddToFavourites";
 
+const API_URL = "https://www.omdbapi.com/?apikey=4159debf&";
+
+const STORAGE_LABEL = "favourite-movie";
 function App() {
+
+  // useState
+  // state includes 
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [favouriteMovies, setFavouriteMovie] = useState([]);
+  const [filters, setFilters] = useState("all");
+
+  // function
+  const getMovieRequest = async(title) => {
+
+    const response = await fetch(`${API_URL}s=${title}`);
+    const responseJson = await response.json();
+
+    if(responseJson.Search) {
+      const newMovies = responseJson.Search.filter((movie) => movie.Poster !== "N/A");
+      setMovies(newMovies);
+      setFilters("all");
+    }
+  }
+
+  //update favourite movies
+  const addFavouriteMovie = (movie) => {
+    const newMovies = [...favouriteMovies, movie];
+    setFavouriteMovie(() => [...favouriteMovies, movie]);
+    saveToLocalStorage(newMovies);
+  }
+
+  //remove favourite movies
+  const removeFavouriteMovie = (movie) => {
+    // if id were matched, remove movie
+    const newMovies = favouriteMovies.filter(favourite => favourite.imdbID !== movie.imdbID);
+    // setFavouriteMovie(favouriteMovies.filter(favourite => favourite.imdbID !== movie.imdbID))
+    setFavouriteMovie(newMovies);
+    saveToLocalStorage(newMovies);
+  }
+
+  // save favourite movies into localStorage
+  const saveToLocalStorage = (movies) => { localStorage.setItem("favourite-movie", JSON.stringify(movies)); } 
+
+  // get movies from localStorage
+  const getFromLocalStorage = () => { 
+    const movieFavourite = JSON.parse(localStorage.getItem(STORAGE_LABEL)); 
+    setFavouriteMovie(movieFavourite); 
+  }
+
+  
+  // useEffect
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+
+  // initialize favourite movie when user does not any type for search movie
+  useEffect(() => {
+    getFromLocalStorage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <MovieListHeading header={"Movies"} />
+      <SearchBox 
+        value={searchValue} 
+        setSearchValue={setSearchValue}/>
+      <Filter filters={filters} setFilters={setFilters}/>
+      {
+        filters === "all" ? 
+        (
+          <MovieList 
+            movies={movies} 
+            favouriteComponent={AddToFavourites}
+            hanldeClickFavourite={addFavouriteMovie}
+            title={"Add to Favourites"}
+        />) : (
+              <MovieList 
+                movies={favouriteMovies} 
+                favouriteComponent={AddToFavourites}
+                hanldeClickFavourite={removeFavouriteMovie} 
+                title={"Remove from Favourites"}/>)
+      }
+      
+      <footer className="footer">
+        <p>nguyenpatz</p>
+        <a href="https://github.com/nguyenpatz">
+          <img src={Github} alt="" />
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
